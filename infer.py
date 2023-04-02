e3219759c37a82b65292bac0cbbfc7a50958e71c
unknown <hugomjp28@gmail.com>
2023-04-02 16:01:58 +0100

    better infer

--- infer.py
+++ infer.py
@@ -1,5 +1,6 @@
 import io
 import time
+import sys
 from pathlib import Path
 
 import librosa
@@ -73,12 +74,13 @@ def run_clip(svc_model, key, acc, use_pe, use_crepe, thre, use_gt_mel, add_noise
 
 if __name__ == '__main__':
     # 工程文件夹名，训练时用的那个
-    project_name = "yilanqiu"
-    model_path = f'./checkpoints/{project_name}/model_ckpt_steps_246000.ckpt'
+    project_name = sys.argv[1]
+    steps = sys.argv[2]
+    model_path = f'./checkpoints/{project_name}/model_ckpt_steps_{steps}.ckpt'
     config_path = f'./checkpoints/{project_name}/config.yaml'
 
     # 支持多个wav/ogg文件，放在raw文件夹下，带扩展名
-    file_names = ["青花瓷.wav"]
+    file_names = [sys.argv[3]]
     trans = [0]  # 音高调整，支持正负（半音），数量与上一行对应，不足的自动按第一个移调参数补齐
     # 加速倍数
     accelerate = 20
